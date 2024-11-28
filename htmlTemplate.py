fe6d9defe97c64ca9487606b591035016c38cdec
Dharani Chandra <dchandra@pdx.edu>
2024-11-27 16:38:34 -0800

    new file added

---
+++ htmlTemplate.py
@@ -0,0 +1,45 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e;
+}
+.chat-message.bot {
+    background-color: #475063;
+}
+.chat-message .avatar {
+  width: 10%;
+}
+.chat-message .avatar img {
+  max-width: 50px;
+  max-height: 50px;
+  border-radius: 50%;
+  vertical-align: middle;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/GkvCSMj/robot.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://i.ibb.co/5jgHxqy/man.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
